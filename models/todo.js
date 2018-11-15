2abc6f0b1f7bdfd86617c5f6c150389dfdacdf48
Loong <longvv@foobla.com>
2018-11-15 16:24:19 +0700

    first commit

---
+++ models/todo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let todoSchema = new Schema({
+   description: {
+       type: String,
+       require: true
+   },
+   done: {
+       type: Boolean,
+       default: false
+   }
+});
+
+module.exports = mongoose.model('Todo', todoSchema);
